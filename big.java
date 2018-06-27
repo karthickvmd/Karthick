b5aea76d6a4c730aaee4e25a05c001890a23f1a6
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-06-27 18:42:26 +0530

    Create big.java

---
+++ big.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class big
+{
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int s=sc.nextInt();
+  if(s==0)
+  {
+  System.out.print("zero");
+  }
+  if(s<0)
+  {
+  System.out.print("negative");
+  }
+  else
+  {
+  System.out.print("positive");
+  }
+  }
+  }
