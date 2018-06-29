5e232a195ad716592ab1d2482b3379af5a927893
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-06-29 23:40:05 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class rev {
+
+	public static void main(String[] args)
+	{
+           Scanner sc=new Scanner(System.in);
+           String s=sc.nextLine();
+           StringBuffer s1=new StringBuffer(s);
+           String s2=s1.reverse().toString();
+           System.out.print(s2);
+
+	}
+
+}
