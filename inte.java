7773c368d563fd43b9f042f8c803d32acbea8dfb
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-06-29 23:52:15 +0530

    Create inte.java

---
+++ inte.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class inte {
+
+	public static void main(String[] args)
+	{
+           Scanner sc=new Scanner(System.in);
+           String s=sc.nextLine();
+           StringBuffer s1=new StringBuffer(s);
+           String s2=s1.reverse().toString();
+           int l=Integer.parseInt(s2);
+           System.out.print(l);
+
+	}
+
+}
