48acf5a172525098ac0e79c7fa774c619539caa1
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-06-30 00:05:23 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class app {
+
+	public static void main(String[] args)
+	{
+           Scanner sc=new Scanner(System.in);
+           String s=sc.nextLine();
+        
+           System.out.print(s+".");
+
+	}
+
+}
