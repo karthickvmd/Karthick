a53e88cf84f81bf919a908687e939cd5cd641ed8
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-06-29 18:37:22 +0530

    Create re.java

---
+++ re.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class re
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1=new StringBuffer(s).reverse().toString();
+System.out.print(s1);
+}
+}
