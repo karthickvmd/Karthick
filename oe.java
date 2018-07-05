9d34bd0c45c84c4c06a7ce2b73416ee24ac5461b
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-07-05 06:52:53 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class oe {
+
+	public static void main(String[] args)
+	{
+           Scanner sc=new Scanner(System.in);
+           int s=sc.nextInt();
+           if(s<0)
+           {
+        	   System.out.print("invalid");
+           }
+           else if(s%2==0)
+           {
+        	    System.out.print("Even");
+           }
+           else
+           {
+        	   System.out.print("Odd");
+           }
+       
+           
+
+	}
+
+}
