5caabf81ad3143fdc1a7dfeca05a1821d01abb57
karthickvmd <40628543+karthickvmd@users.noreply.github.com>
2018-07-05 07:19:51 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,35 @@
+package hello;
+import java.util.*;
+
+public class holiday {
+
+	public static void main(String[] args)
+	{
+           Scanner sc=new Scanner(System.in);
+           String s=sc.nextLine();
+             String k="Sunday";
+             String k1="Saturday";
+           
+        	   
+               if(s.equals(k))
+           
+        	   
+        	   {
+        	   System.out.print("Yes");
+        	
+                }
+               else if(s.equals(k1)) 
+               {
+            	   System.out.print("Yes");
+               }
+               else
+           {
+        	   System.out.print("No");
+           }
+           
+       
+           
+
+	}
+
+}
